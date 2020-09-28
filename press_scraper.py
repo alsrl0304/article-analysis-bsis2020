b471f0bb6cbc58b96d4e238bca8d2a339b84144a
alsrl0304 <alsrl0304@gmail.com>
2020-09-28 16:02:36 +0900

    add scraper files via upload
    미리 작업된 스크래퍼 소스 파일 및 크롬드라이버(예비용) 업로드

---
+++ press_scraper.py
@@ -0,0 +1,204 @@
+##########################################################################
+# 특정 언론사 웹 페이지에서 기사 스크랩하는 클래스
+##########################################################################
+
+
+import sys  # 시스템 모듈
+import re  # 정규표현식
+from functools import reduce  # 고차함수
+from overrides import overrides  # 오버라이드 데코레이터
+
+import requests  # HTTP REQUEST를 위한 모듈
+from bs4 import BeautifulSoup  # HTML 분석기
+
+# 브라우저 자동화 도구
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+
+class Scraper:
+    """
+    기사 스크래퍼를 위한 추상 클래스
+    """
+
+    CHARACTER_FILTER = re.compile(
+        r'[^ 가-힣|a-z|A-Z|0-9|\[|\]|(|)|-|~|?|!|.|,|:|;|%]+')  # 특수 문자나 필요없는 문자들을 제외하기 위한 정규식
+
+    DATE_REGEX = re.compile(
+        r'.*((?:19|20)(?:\d{2}))[-.](0[1-9]|1[0-2])[-.]([012][0-9]|3[01]).*')  # 날짜 검출용 정규식
+
+    @staticmethod
+    def _extract_date(text):  # 정규식으로 날짜만 분리
+        match = re.search(Scraper.DATE_REGEX, text)  # 텍스트에서 정규식 매치
+        # 그룹으로 매치된 날짜 (YYYY-MM-DD) 반환
+        return f'{match.group(1)}-{match.group(2)}-{match.group(3)}'
+
+    @staticmethod
+    def _clean_text(text):  # 필요한 문자만 남기기 위한 함수
+        return re.sub(r' +', ' ', Scraper.CHARACTER_FILTER.sub(' ', text))
+
+    @staticmethod
+    def _request_get(href):  # HTTP GET 함수
+        try:
+            response = requests.get(href)  # HTTP GET
+            response.encoding = None  # 한글 깨짐을 방지하기 위한 인코딩 자동 변환 방지
+            return response
+        except requests.exceptions.HTTPError as error:
+            print(error)
+            sys.exit(1)
+        except requests.exceptions.InvalidURL as error:
+            print(error)
+            sys.exit(1)
+
+    @staticmethod
+    def _get_soup(source):  # 링크로 HTTP GET한 HTML문서로부터 BS4 객체를 얻어옴
+        return BeautifulSoup(source, 'html.parser')
+
+    # {'href': (링크), 'title': (제목)} 딕셔너리의 리스트 형식으로 반환할것
+    def collect_articles(self, number_of_articles, query_word, detail_word):
+        raise NotImplementedError
+
+    # {'date': (날짜), 'title': (제목), 'body': (내용)} 딕셔너리로 반환할것
+    def scrap_articles(self, article_href):
+        raise NotImplementedError
+
+
+class JoongangScraper(Scraper):
+    """
+    중앙일보용 스크래퍼
+    """
+
+    @overrides
+    def collect_articles(self, number_of_articles, query_word, detail_word):
+        ARTICLES_PER_PAGE = 10  # 페이지당 기사 수
+        article_list = []
+
+        num = 0  # 찾은 기사 수
+        for page in range(1, (number_of_articles // ARTICLES_PER_PAGE) + 2):
+
+            soup = Scraper._get_soup(
+                Scraper._request_get(
+                    f'https://news.joins.com/search/JoongangNews?page={page}&Keyword={query_word}&SortType=New&SearchCategoryType=JoongangNews&IncludeKeyword={detail_word}')
+                .text)
+            # 중앙일보 웹 서버로 HTTP GET
+            # 상세 검색 기능도 함께 이용하여 IncludeKeyword에 명시된 키워드를 포함하는 기사만 검색
+
+            link_elements = soup.select(
+                '#content > div.section_news > div.bd > ul > li > div > h2 > a')
+            # 검색 결과의 제목과 사이트 주소가 포함되어 있는 부분의 css selector. 이 실렉터로 해당 데이터 가져옴
+            # 리스트 형식으로 여러개 반환
+            # 검색 결과가 여러 개(중앙일보 사이트는 10개)인 경우 리스트 요소들로부터 하나씩 불러와서 작업하기 위한 반복문
+
+            for element in link_elements:
+                if num >= number_of_articles:
+                    break
+                article = {'href': element.get(
+                    "href"), 'title': Scraper._clean_text(element.get_text())}
+                article_list.append(article)
+                num = num + 1
+                print(num, article['href'], sep=': ')
+
+        return article_list
+
+    @overrides
+    def scrap_articles(self, article_href):
+
+        soup = Scraper._get_soup(Scraper._request_get(article_href).text)
+
+        # 기사 날짜 추출
+        date_element = soup.select(
+            'div.article_head > div.clearfx > div.byline > em')[1]  # 최초 일자 요소 (최종 수정일자는 [2]번째 요소)
+        date = Scraper._extract_date(
+            date_element.get_text())  # 정규식으로 날짜만 얻어옴
+
+        # 기사 제목 추출
+        title_element = soup.select('#article_title')[0]  # 기사 제목 요소 추출. 하나뿐임
+        title = Scraper._clean_text(title_element.get_text())
+
+        # 기사 본문 추출하는 부분
+        body_elements = soup.select(
+            '#article_body')  # 기사 내용 요소 추출, 여러개일 수 있음
+
+        body = reduce(lambda prev, next: prev + next,
+                      map(lambda element: Scraper._clean_text(element.get_text()), body_elements))  # 각 요소별 내용을 하나로 합침
+
+        # {'date': (날짜), 'title': (제목), 'body': (내용)} 딕셔너리로 반환
+        return {'date': date, 'title': title, 'body': body}
+
+
+class DongaScraper(Scraper):
+    """
+    동아일보용 스크래퍼
+    """
+
+    def __init__(self, path_chromedriver):
+        self.path_chromedriver = path_chromedriver
+
+    @overrides
+    def collect_articles(self, number_of_articles, query_word, detail_word):
+        # 기사 링크만 필터링
+        ARTICLE_HREF_FILTER = re.compile(r'.+/news/article/.+')
+        ARTICLES_PER_PAGE = 15  # 페이지당 기사 수
+        article_list = []
+
+        # Selenium 설정
+        options = webdriver.ChromeOptions()  # Chromedriver 옵션
+        options.add_argument('headless')  # 헤드리스(GUI 없음) 모드
+        options.add_argument('window-size=1920x1080')
+        options.add_argument("disable-gpu")
+        options.add_argument("--log-level=3")
+        driver = webdriver.Chrome(
+            self.path_chromedriver, chrome_options=options)
+
+        num = 0  # 찾은 기사 수
+        for page in range(0, (number_of_articles // ARTICLES_PER_PAGE) + 1):
+            driver.get(
+                f'https://www.donga.com/news/search?p={1+page*ARTICLES_PER_PAGE}&query={query_word}&check_news=1&more=1&sorting=1&search_date=1&v1=&v2=&range=1')
+
+            WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((
+                By.CSS_SELECTOR, '#content > div.searchContWrap > div.searchCont > div.searchList > div.t > p.tit > a')))
+
+            soup = Scraper._get_soup(driver.page_source)
+
+            link_elements = soup.select(
+                '#content > div.searchContWrap > div.searchCont > div.searchList > div.t > p.tit > a')  # 검색 결과의 제목과 사이트 주소가 포함되어 있는 부분의 css selector.
+
+            link_elements = list(filter(lambda element: ARTICLE_HREF_FILTER.search(
+                element.get("href")), link_elements))  # 기사 링크만 필터링
+
+            for element in link_elements:
+                if num >= number_of_articles:
+                    break
+                article = {'href': element.get(
+                    "href"), 'title': Scraper._clean_text(element.get_text())}
+                article_list.append(article)
+                num = num + 1
+                print(num, article['href'], sep=': ')
+
+        return article_list
+
+    @ overrides
+    def scrap_articles(self, article_href):
+        soup = Scraper._get_soup(Scraper._request_get(article_href).text)
+
+        # 기사 날짜 추출
+        date_element = soup.select(
+            '#container > div.article_title > div.title_foot > span.date01')[0]  # 최초 일자 요소 (최종 수정일자는 [1]번째 요소)
+        date = Scraper._extract_date(
+            date_element.get_text())  # 정규식으로 날짜만 얻어옴
+
+        # 기사 제목 추출
+        title_element = soup.select(
+            '#container > div.article_title > h1')[0]  # 기사 제목 요소 추출. 하나임
+        title = Scraper._clean_text(title_element.get_text())
+
+        # 기사 본문 추출하는 부분
+        body_element = soup.select(
+            '#content > div > div.article_txt')[0]  # 기사 내용 요소 추출, 하나임
+
+        body = Scraper._clean_text(body_element.get_text())
+
+        # {'date': (날짜), 'title': (제목), 'content': (내용)} 딕셔너리로 반환
+        return {'date': date, 'title': title, 'body': body}
---
+++ scrap_articles.py
@@ -0,0 +1,160 @@
+###################################################################################
+# 특정 언론사 웹 페이지에서 기사 목록(링크)를 스크랩함
+# 한 줄에 하나씩 (링크), (제목) 형식으로 기록함
+#
+# 사용법: [-h] [-p <press>] [-c -n <number> -q <query> -d <detail>] [-s -o <output>] [-l <list]
+# -h --help: 도움말
+# -p --press [언론] [joongang | donga]
+# -c --collect: 기사 목록 검색
+#     -n --number [찾을 기사 수]
+#     -s --search [주 검색어]
+#     -d --detail [부가 포함 검색어]
+#     -l --list   (저장할 파일명)
+#
+# -s --scrap 기사 내용 스크랩
+#     -l --list   (기사 목록 파일명)
+#     -r --result [출력 파일명]
+#
+##################################################################################
+
+import sys  # 시스템 모듈
+import datetime  # 시각 모듈
+import getopt  # 명령행 인수 파서
+import inspect  # 텍스트 도구
+import csv  # csv 파서
+from press_scraper import JoongangScraper, DongaScraper  # 링크 스크래퍼 클래스
+
+
+def print_help(exit_code):
+    """
+    커맨드라인 도움말
+    """
+    print(inspect.cleandoc('''사용법: [-h] [-p <press>] [-c -n <number> -q <query> -d <detail>]
+                                      [-s -o <output>] [-l <list>]
+                              -h --help: 도움말
+                              -p --press [언론] [joongang | donga]
+                              -c --collect: 기사 목록 검색
+                                  -n --number [찾을 기사 수]
+                                  -q --query [주 검색어]
+                                  -d --detail (부가 포함 검색어)
+                                  -l --list   (저장할 파일명)
+
+                              -s --scrap 기사 내용 스크랩
+                                  -l --list   (기사 목록 파일명)
+                                  -r --result (출력 파일명)'''))
+    sys.exit(exit_code)
+
+
+def main(argv):
+    """
+    메인 루틴
+    """
+
+    press = None  # 언론
+
+    collect_article = False  # 기사 수집작업
+    number_of_articles = None  # 찾을 기사 수
+    query_word = None  # 주 검색어
+    detail_word = ''  # 부 검색어
+
+    scrap_article = False  # 기사 스크랩 작업
+    result_file_name = None  # 결과 파일명
+
+    list_file_name = None  # 기사 리스트 파일명
+
+    try:  # 명령행 인수 파싱
+        opts, _ = getopt.getopt(argv, 'hp:cn:q:d:sr:l:', [
+            'help', 'press=', 'collect', 'number=', 'query=', 'detail=', 'scrap', 'result=', 'list='])
+
+    except getopt.GetoptError as error:  # 오류 발생 (잘못된 입력)
+        print(error)
+        print_help(1)
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):  # 도움말
+            print_help(0)
+        elif opt in ('-p', '--press'):  # 언론
+            if arg in ('joongang', 'donga'):
+                press = arg
+            else:
+                print_help(1)
+        elif opt in ('-c', '--collect'):  # 기사 목록 수집
+            collect_article = True
+        elif opt in ('-n', '--number'):  # 기사 수
+            number_of_articles = int(arg)
+        elif opt in ('-q', '--query'):  # 주 검색어
+            query_word = arg
+        elif opt in ('-d', '--detail'):  # 부 검색어
+            detail_word = arg
+        elif opt in ('-s', '--scrap'):  # 기사 내용 스크랩
+            scrap_article = True
+        elif opt in ('-r', '--result'):  # 결과 파일명
+            result_file_name = arg
+        elif opt in ('-l', '--list'):  # 리스트 파일명
+            list_file_name = arg
+        else:
+            print_help(1)
+
+    # 옵션 확인
+    if collect_article is True:
+        if (number_of_articles is None) or (query_word is None):  # 기사 수, 주 검색어 입력 안함
+            print_help(1)
+
+    if scrap_article is True:
+        if (collect_article is False) and (list_file_name is None):  # 수집 작업 없이 리스트 파일 입력 안함
+            print_help(1)
+
+    if (collect_article is False) and (scrap_article is False):  # 작업 선택 안함
+        print_help(1)
+
+    # 작업 진행
+
+    # 스크래퍼 선택
+    if press == 'joongang':
+        scraper = JoongangScraper()
+    elif press == 'donga':
+        scraper = DongaScraper('./chromedriver.exe')
+    else:
+        print_help(1)
+
+    if collect_article is True:  # 기사 수집
+        article_list = scraper.collect_articles(
+            number_of_articles, query_word, detail_word)
+
+        # 리스트 저장 여부 확인
+        if scrap_article is False and list_file_name is None:  # 스크랩 작업 없이 리스트 파일 출력 안함
+            list_file_name = f'articles_list_{press}_{query_word}_{detail_word}.csv'
+        if list_file_name is not None:  # 리스트 파일 지정
+            file_list = open(list_file_name, 'w', encoding='utf8')  # 리스트 파일 생성
+            for article in article_list:
+                row = f'{article["href"]}, "{article["title"]}"\n'
+                file_list.write(row)  # 기록
+            file_list.close()  # 스트림 종료
+
+    if scrap_article is True:  # 기사 스크랩
+        if collect_article is not True:  # 기사 수집 안함, 리스트 파일 필요
+            file_list = open(list_file_name, 'r', encoding='utf8')  # 리스트 파일 읽음
+            article_list = csv.reader(file_list)  # csv 파싱
+
+        if result_file_name is None:  # 기본 출력 파일명 지정
+            result_file_name = f'article_scrap_{datetime.datetime.now().strftime("%Y-%m-%d")}.csv'
+
+        file_result = open(result_file_name, 'w', encoding='utf8')  # 결과 파일 생성
+        file_result.write('"adate", "atitle", "article"\n')  # R 호환 헤더
+
+        num = 1  # 횟수 카운터
+        for article in article_list:
+            href = article[0]
+            content = scraper.scrap_articles(href)  # 내용 스크랩
+            row = f'"{content["date"]}", "{content["title"]}", "{content["body"]}"\n'
+            file_result.write(row)  # 파일 작성
+
+            # 작업 상황 출력
+            print(num, href, sep=': ')
+            num = num + 1
+
+        file_result.close()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
